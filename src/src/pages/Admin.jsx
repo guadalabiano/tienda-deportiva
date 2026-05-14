633e4b5c78013c8c04c9306693cf0f1b38b8a18e
Guada Labiano <guada.labiano05@gmail.com>
2026-05-14 10:26:22 -0300

    Estructura de carpetas y enrutamiento con react-router-dom

---
+++ src/src/pages/Admin.jsx
@@ -0,0 +1,3 @@
+export default function Admin() {
+  return <h1>Página de Admin</h1>;
+}
---
+++ src/src/pages/DetalleProducto.jsx
@@ -0,0 +1,3 @@
+export default function DetalleProducto() {
+  return <h1>Página de Detalle de Producto</h1>;
+}
