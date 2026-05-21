ac7d649dc95669ccc8e6cbc55501688003d65ffd
Maximo Rodriguez <maximor940@gmail.com>
2026-05-20 21:29:03 -0300

    Crear formulario de login simulado y actualizar rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import DetalleProducto from './pages/DetalleProducto';
-import Carrito from './pages/Carrito';
-import Admin from './pages/Admin';
+import Home from './pages/Home.jsx';
+import DetalleProducto from './pages/DetalleProducto.jsx';
+import Carrito from './pages/Carrito.jsx';
+import Login from './pages/Login.jsx';
+import Admin from './pages/Admin.jsx';
 
 function App() {
   return (
     <Router>
       <nav style={{ padding: '1rem', background: '#333', color: 'white' }}>
-        {/* Este es un Navbar temporal para que pruebes que funciona */}
+       
         <Link to="/" style={{ marginRight: '10px', color: 'white' }}>Inicio</Link>
         <Link to="/carrito" style={{ marginRight: '10px', color: 'white' }}>Carrito</Link>
         <Link to="/admin" style={{ color: 'white' }}>Panel Admin</Link>
+        <Link to="/login" style={{marginLeft: '10px', color: 'lightblue' }}> Iniciar Sesión</Link>
       </nav>
 
       <div style={{ padding: '2rem' }}>
@@ -20,10 +22,12 @@ function App() {
           <Route path="/producto/:id" element={<DetalleProducto />} />
           <Route path="/carrito" element={<Carrito />} />
           <Route path="/admin" element={<Admin />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
   );
+  
 }
 
 export default App;
